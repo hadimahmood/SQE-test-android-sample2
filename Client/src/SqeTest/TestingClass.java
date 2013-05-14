158e27dc508fb2d054304bab87a8f82b43005f61
Hadi Mahmood <10besehmahmood@Seecs.edu.pk>
2013-05-14 23:58:32 +0500

    No new things

---
+++ Client/src/SqeTest/TestingClass.java
@@ -0,0 +1,11 @@
+package SqeTest;
+
+public class TestingClass {
+    public TestingClass() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        TestingClass testingClass = new TestingClass();
+    }
+}
