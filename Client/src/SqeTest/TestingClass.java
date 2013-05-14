4166a6856f03c7fa63f55cbd4011038e28741924
Hadi Mahmood <10besehmahmood@Seecs.edu.pk>
2013-05-15 00:05:36 +0500

    Added output code to TestingClass.Java

--- Client/src/SqeTest/TestingClass.java
+++ Client/src/SqeTest/TestingClass.java
@@ -1,11 +1,16 @@
 package SqeTest;
 
+import java.io.Console;
+
 public class TestingClass {
+    
     public TestingClass() {
         super();
     }
 
     public static void main(String[] args) {
         TestingClass testingClass = new TestingClass();
+        Console console = System.console();
+        System.out.println("This is a test program.");
     }
 }
